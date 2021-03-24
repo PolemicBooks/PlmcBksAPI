159ce64badd5bff60766a071573cff27744e2024
SnwMds <46784020+SnwMds@users.noreply.github.com>
2021-03-24 15:34:59 -0300

    improve opds feed

--- config/feeds/opds.py
+++ config/feeds/opds.py
@@ -2,17 +2,19 @@ BASE = """\
 <?xml version="1.0" encoding="utf-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom"
       xmlns:dc="http://purl.org/dc/terms/"
-      xmlns:opds="http://opds-spec.org/2010/catalog">
+      xmlns:opds="http://opds-spec.org/2010/catalog"
+      xmlns:opensearch="http://a9.com/-/spec/opensearch/1.1/">
   <title>{}</title>
   <updated>{}</updated>
   <link rel="http://opds-spec.org/image"
       href="{}"
       type="image/jpeg"/>
+  <icon>https://polemicbooks.github.io/images/polemicbooks.jpg</icon>
   <link type="application/atom+xml" title="Buscar no Polemic Books" href="/opds/search/books?query_name={{searchTerms}}" rel="search"/>
   <author>
     <name>Polemic Books</name>
     <uri>https://t.me/PolemicBooks</uri>
-    <email>46784020+SnwMds@users.noreply.github.com</email>
+    <email>plmcbks@pm.me</email>
   </author>
   <subtitle>{}</subtitle>
 """
