8ff32487013f71b8c46c10158301324400e71b44
SnwMds <46784020+SnwMds@users.noreply.github.com>
2021-04-15 12:41:58 -0300

    remove some headers

--- config/headers/headers.py
+++ config/headers/headers.py
@@ -4,7 +4,6 @@ HTTP_HEADERS = [
 	("Access-Control-Allow-Origin", "*"),
 	("Access-Control-Expose-Headers", "*"),
 	("Access-Control-Max-Age", "3600"),
-	("Content-Language", "en-US"),
 	("Cache-Control", "public, max-age=3600"),
 	("Cross-Origin-Embedder-Policy", "require-corp"),
 	("Cross-Origin-Opener-Policy", "same-origin"),
@@ -13,7 +12,5 @@ HTTP_HEADERS = [
 	("Referrer-Policy", "no-referrer"),
 	("Content-Security-Policy", "default-src 'none'; connect-src 'self'; img-src https://fastapi.tiangolo.com; script-src 'unsafe-inline' https://cdn.jsdelivr.net; style-src https://cdn.jsdelivr.net; frame-ancestors 'none'; form-action 'none'; navigate-to 'none'"),
 	("Accept-Ranges", "none"),
-	("X-Robots-Tag", "noindex, nofollow, noarchive, nocache, noimageindex, noodp"),
-	("Strict-Transport-Security", "max-age=31536000"),
-	("Alt-Svc", 'http/1.1=":443"; ma=2592000; persist=1; h2=":443"; ma=2592000; persist=1')
+	("X-Robots-Tag", "noindex, nofollow, noarchive, nocache, noimageindex, noodp")
 ]
