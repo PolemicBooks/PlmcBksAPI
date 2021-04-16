6edba65b6316e00fdd04c20f8539df9c59f1b9eb
SnwMds <46784020+SnwMds@users.noreply.github.com>
2021-04-15 21:20:01 -0300

    update url

--- config/urls/urls.py
+++ config/urls/urls.py
@@ -2,7 +2,7 @@ from urllib.parse import urlparse
 
 
 # Endereço da API
-API_URL = "https://api.polemicnews.com"
+API_URL = "https://plmcbks.amanoteam.com"
 
 # Link para o chat onde estão os livros
 PRIVATE_CHAT_URL = "https://t.me/c/1436494509"
